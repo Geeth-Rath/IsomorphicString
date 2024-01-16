8d9856816550983feb8483d7c54ac98aaa25bbc4
Geeth-Rath <geeth4862@gmail.com>
2024-01-16 14:12:58 +0530

    isomorphic String is done

---
+++ src/main/java/org/example/Isomorphic.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Isomorphic {
+    public boolean isIsomorphic(String s, String e) {
+        char[] sArry = s.toCharArray();
+        char[] eArry = e.toCharArray();
+        int i = 0;
+
+        Map<Character, Character> hmap = new HashMap<>();
+        if (sArry.length == eArry.length) {
+
+            while (i <= sArry.length - 1) {
+                if (hmap.isEmpty()) {
+                    hmap.put(sArry[i], eArry[i]);
+                } else if (hmap.containsKey(sArry[i]) && hmap.get(sArry[i]) != eArry[i]) {
+                    return false;
+                } else {
+                    hmap.put(sArry[i], eArry[i]);
+                }
+                i++;
+
+            }
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Isomorphic s = new Isomorphic();
+        boolean flag = s.isIsomorphic("avb", "xyz");
+        System.out.println(flag);
+
+    }
+}
